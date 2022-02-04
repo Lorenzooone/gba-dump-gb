bb91d860049e98948f7f35cae53fe27e6ecff606
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2022-02-04 15:01:18 +0100

    Confirm cartridge is valid

--- source/gbc_payload/payload.asm
+++ source/gbc_payload/payload.asm
@@ -16,34 +16,60 @@ start:
     jr  nz,.copy_hram_loop
 
 .jump_to_hram
-    ld  hl,$0101                       ; chosen target byte
     jp  $FF80
 
 .hram_section
     ld  a,LCDCF_ON
     ld  [rLCDC],a
 .main_loop
-    ld  b,$C
 .inner_loop
     xor a
     ld  [rIF],a
     inc a
     ld  [rIE],a
     jr  .wait_interrupt
-.reproduce_sound
+    
+.check_logo
+    ld  hl,$0104                       ; Start of the Nintendo logo
+    ld  b,$30                          ; Nintendo logo's size
+    ld  c,$80+logoData-.hram_section
+.check_logo_loop
+    ld  a,[$ff00+c]
+    cp  [hl]
+    jr  nz,.main_loop
+    inc c
+    inc hl
     dec b
-    jr  nz,.inner_loop
+    jr  nz,.check_logo_loop
+    
+.check_header
+    ld  b,$19
+    ld  a,b
+.check_header_loop
+    add [hl]
+    inc l
+    dec b
+    jr  nz,.check_header_loop
+    add [hl]
+    jr  nz,.main_loop
+
+.success
     ld  a,$FF
     ld  [rNR50],a
     ld  [rNR51],a
     ld  [rNR52],a
-    ld  a,[hl]
+    ld  a,$CE
     ld  [rNR12],a
     ld  [rNR13],a
     ld  [rNR14],a
     jr  .main_loop
+    
 .wait_interrupt
     ld  a,[rIF]
     and a,$1
     jr  z,.wait_interrupt
-    jr  .reproduce_sound
+    jr  .check_logo
+
+    SECTION "LOGO",ROM0
+logoData:
+INCBIN "logo.bin"
