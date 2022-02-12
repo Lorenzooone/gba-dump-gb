26477b7adc167b579c05ace924ca7d9a3b827980
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2022-02-12 19:39:24 +0100

    Fix weird behaviour

--- source/gbc_payload/payload.asm
+++ source/gbc_payload/payload.asm
@@ -7,8 +7,8 @@ start:
     ld  [rLCDC],a
 	ld	[rSCX],a
 	ld	[rSCY],a
-    ld  sp,$FFFC                       ; setup stack
-    ld  a,$30                          ; read P15 - returns a, b, select, start
+    ld  sp,$FFFE                       ; setup stack
+    ld  a,$10                          ; read P15 - returns a, b, select, start
     ld  [rP1],a
     ld  a,$80
     ld  [rBCPS],a
@@ -60,80 +60,10 @@ start:
     jr  nz,.copy_hram_loop
 
 .jump_to_hram
-    xor a
-    ld  [$FFFD],a
-    ld  [$FFFE],a
+    ld  de,$0000
     jp  $FF80
-    
-.copy_to_hram2
-    ld  hl,_VRAM+.start_comunication
-    jr  .copy_to_hram_exec
-    
-.start_comunication
-    xor a
-    ld  hl,$FE00
-    ld  c,$A0
-.blank_oam
-    ld  [hl+],a
-    dec c
-    jr nz,.blank_oam
-    ld  a,$04
-    ld  [$FF4C],a                      ; set as DMG
-    ld  a,$01
-    ld  [$FF6C],a                      ; set as DMG
-    xor a
-    ld  [$FF70],a
-    ld  a,$11
-    ld  [$FF50],a                      ; set as DMG
-    ld  a,$91
-    ld  [rLCDC],a
-    jp  $0100
-    
-.ret
-    ret
-    
-.read_input
-    ld  a,$10                          ; read P15 - returns a, b, select, start
-    ld  [rP1],a
-    ld  a,[rP1]                        ; read input
-    cpl
-    and a,PADF_A|PADF_B|PADF_START|PADF_SELECT
-    jr  z,.ret
-    rla
-    ld  d,$00
-    ld  e,a
-    ld  hl,_VRAM+inputData
-    add hl,de
-    ld  a,[hl+]
-    ld  d,a
-    ld  a,[hl+]
-    ld  e,a
-    
-.update_val
-    ld  a,[$FFFD]
-    ld  h,a
-    ld  a,[$FFFE]
-    ld  l,a
-    add hl,de
-    ld  a,h
-    ld  [$FFFD],a
-    ld  a,l
-    ld  [$FFFE],a
-    ret
-    
-.render_number
-    ld  a,b
-    swap a
-    and a,$0F
-    add a,$80
-    ld  [hl+],a
-    ld  a,b
-    and a,$0F
-    add a,$80
-    ld  [hl+],a
-    ret
 
-SECTION "HRAM",ROM0
+SECTION "HRAM",ROM0[$600]
 hram_code:
     ld  a,LCDCF_ON | LCDCF_BG8000 | LCDCF_BG9C00 | LCDCF_OBJ8 | LCDCF_OBJOFF | LCDCF_WINOFF | LCDCF_BGON
     ld  [rLCDC],a
@@ -143,13 +73,66 @@ hram_code:
     ld  [rIF],a
     inc a
     ld  [rIE],a
-    jr  .wait_interrupt
+.wait_interrupt
+    ld  a,[rIF]
+    and a,$1
+    jr  z,.wait_interrupt
     
 .render_and_input
-    call $FF80+.change_arrangements-hram_code
+    ld  hl,$9C00
+    ld  b,$12/2
+    push de
+.change_arrangements_row
+    ld a,d
+    call $FF80-hram_code+.render_number
+    ld a,e
+    call $FF80-hram_code+.render_number
+    ld  c,$4
+.change_arrangements_single
+    inc hl
+    ld  a,[de]
+    inc de
+    call $FF80-hram_code+.render_number
+    dec c
+    jr  nz,.change_arrangements_single
+    ld  c,$30
+.reach_next_row
+    inc hl
+    dec c
+    jr  nz,.reach_next_row
+    dec b
+    jr  nz,.change_arrangements_row
+    pop de
+.read_input
+    ld  a,[rP1]                        ; read input
+    cpl
+    and a,PADF_A|PADF_B|PADF_START|PADF_SELECT
+    rla
+    ld  h,(_VRAM+inputData)/$100
+    ld  l,a
     call $FF80-hram_code+.wait_VRAM_accessible
-    call _VRAM+start.read_input
+    ld  a,[hl+]
+    ld  b,a
+    ld  a,[hl]
+    ld  l,a
+    ld  h,b
+    add hl,de
+    ld  d,h
+    ld  e,l
     jr  .main_loop
+    
+.render_number
+    push af
+    swap a
+    and a,$0F
+    add a,$80
+    call $FF80-hram_code+.wait_VRAM_accessible
+    ld  [hl+],a
+    pop af
+    and a,$0F
+    add a,$80
+    ld  [hl+],a
+    ret
     
 .wait_VRAM_accessible
     push hl
@@ -159,56 +142,6 @@ hram_code:
     jr  nz,.wait
     pop hl
     ret
-
-.change_arrangements
-    ld  hl,$9C00
-    ld  b,$12/2
-    ld  a,[$FFFD]
-    ld  d,a
-    ld  a,[$FFFE]
-    ld  e,a
-.change_arrangements_row
-    call $FF80-hram_code+.render_raw_number
-    ld  c,$4
-.change_arrangements_single
-    inc hl
-    call $FF80-hram_code+.render_load_number
-    dec c
-    jr  nz,.change_arrangements_single
-    push de
-    ld  de,$30
-    add hl,de
-    pop de
-    dec b
-    jr  nz,.change_arrangements_row
-    ret
-    
-.render_load_number
-    push bc
-    ld  a,[de]
-    ld  b,a
-    call $FF80-hram_code+.wait_VRAM_accessible
-    call _VRAM+start.render_number
-    inc de
-    pop bc
-    ret
-    
-.render_raw_number
-    push bc
-    ld b,d
-    call $FF80-hram_code+.wait_VRAM_accessible
-    call _VRAM+start.render_number
-    ld b,e
-    call $FF80-hram_code+.wait_VRAM_accessible
-    call _VRAM+start.render_number
-    pop bc
-    ret
-
-.wait_interrupt
-    ld  a,[rIF]
-    and a,$1
-    jr  z,.wait_interrupt
-    jr  .render_and_input
     
 .end_hram_code
 ASSERT (.end_hram_code - hram_code) < ($72) ; calling functions consumes a bit of the available space
@@ -221,7 +154,7 @@ INCBIN "logo.bin"
 emptyTile:
 DB $10+$80
 
-    SECTION "Input_Data",ROM0
+    SECTION "Input_Data",ROM0[$700]
 inputData:
 INCBIN "sum_values.bin"
 
