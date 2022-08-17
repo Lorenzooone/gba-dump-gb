4b40a720251932584721d6cfc7858a065b91b0f7
Lorenzo Carletti <lollo.lollo.rbiz@gmail.com>
2022-08-17 05:00:21 +0200

    Force alignment

--- switch_to_dump/source/gbc_payload/convert_to_c.py
+++ switch_to_dump/source/gbc_payload/convert_to_c.py
@@ -3,7 +3,7 @@ from functools import partial
 
 if len(sys.argv) < 4:
   sys.exit('Usage: %s file limit align' % sys.argv[0])
-prod = "const uint8_t payload[PAYLOAD_SIZE] = {\n    "
+prod = "const uint8_t payload[PAYLOAD_SIZE] __attribute__ ((aligned (16))) = {\n    "
 n = 0
 with open(sys.argv[1], "rb") as in_file:
   for c in iter(partial(in_file.read, 1), b''):
